068ed769d71329ccf69976900d7e79a8374f1fd5
ziad hany <zizo.juv7@gmail.com>
2025-06-30 14:52:51 +0300

    firebase key issue

--- Src/Modules/Notification/utils/firebase.initial.js
+++ Src/Modules/Notification/utils/firebase.initial.js
@@ -2,6 +2,9 @@ import admin from "firebase-admin";
 import dotenv from "dotenv"
 dotenv.config()
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+
+firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig)
 });
